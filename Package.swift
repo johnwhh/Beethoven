efb9b240556c6bd9a05e0dccc428f78613172d3b
John Holtzworth <johnwhh@icloud.com>
2024-05-02 19:51:06 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,19 @@ import PackageDescription
 
 let package = Package(
   name: "Beethoven",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(name: "Beethoven", targets: ["Beethoven-iOS"])
+  ],
   dependencies: [
     .package(url: "https://github.com/vadymmarkov/Pitchy.git", from: "2.0.0")
+  ],
+  targets: [
+    .target(
+        name: "Beethoven-iOS",
+        dependencies: [
+            .product(name: "Pitchy")
+        ]
+    )
   ]
 )
